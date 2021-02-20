fcf1fded712162e11190d8996333e69e220f2f22
DARKDRAGON532 <69623818+DARKDRAGON532@users.noreply.github.com>
2021-02-20 17:25:03 +0530

    fixed

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,13 +1,12 @@
-import random
-random_number = random.randint(0, 100)
-print('There is a random number between 1 - 100')
-nice = True
-while nice:
+from random import randint
+while True:
+    print('There is a random number between 1 - 100')
+    random_number = randint(0, 100)
     guess = int(input('Guess the number > '))
     if guess == random_number:
         print('WoW you won :D')
         break
     elif guess < random_number:
         print('Too low :(')
-    elif guess > random_number:
+    else:
         print('Too high :(')
