a3d248f97faa0d01972b81e021223eb7cee4cf7f
Limeissour <77962504+Limeissour@users.noreply.github.com>
2021-03-08 16:20:33 +0530

    Revert "fixed"

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,12 +1,13 @@
-from random import randint
-while True:
-    print('There is a random number between 1 - 100')
-    random_number = randint(0, 100)
+import random
+random_number = random.randint(0, 100)
+print('There is a random number between 1 - 100')
+nice = True
+while nice:
     guess = int(input('Guess the number > '))
     if guess == random_number:
         print('WoW you won :D')
         break
     elif guess < random_number:
         print('Too low :(')
-    else:
+    elif guess > random_number:
         print('Too high :(')
